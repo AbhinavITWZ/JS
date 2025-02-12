b72833470eb66cc7a3c397f92e6d18156dbf0062
Abhinav Kumar Jha <coderabhinavjha@gmail.com>
2025-02-12 10:47:42 +0530

    control flow files

---
+++ JS/controlFlow.js
@@ -0,0 +1,43 @@
+//if    else if    else   ...... conditionals
+//Loops .......   for,while,do while
+//switch case 
+
+
+//print even numbers from 1 to upto user demand
+let num=1
+while(num<=12){
+    if(num %2==0){
+        console.log(num);
+        
+    }
+    num++;
+}
+
+
+
+for(let i=1;i<=12;i++){
+    if(i %2==0){
+        console.log(i);
+        
+    }
+}
+
+
+switch(0){
+    case 1:
+        console.log("Hello 1"); break;
+    case 5:
+            console.log("Hello 5"); break;
+     case 4:
+        console.log("Hello 4"); break;
+     case 3:
+            console.log("Hello 3"); break;
+     case 2:
+            console.log("Hello 2"); break;
+    default:
+        console.log("wow"); 
+        
+    
+        
+}
+
